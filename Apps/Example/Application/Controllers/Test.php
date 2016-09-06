314f1e9b82c3b4627e1b0df83b5df2a508788519
beck917 <beck917@gmail.com>
2016-09-06 18:35:50 +0800

    no message

--- Apps/Example/Application/Controllers/Test.php
+++ Apps/Example/Application/Controllers/Test.php
@@ -14,18 +14,20 @@ class Test extends \Phpill\Libraries\Controller {
 	public function test1()
 	{
 
-		echo "test";
-		$match_model = new \Application\Example\Models\Match(); 
-		$match_model->getMatchId(6534);
+		echo "test";  
+		//$match_model = new \Application\Example\Models\Match(); 
+		//$match_model->getMatchId(6534);
+		
+		echo \Modules\Core\Helpers\Common::getUUID();
 		
 		//$cache = \System\Libraries\Cache::instance();
 		//$cache->set("testabc_ph", "test2");
 		
 		//echo $cache->get("testabc_ph");
 
-		$log = new \Monolog\Logger('name');
-		$data = new \Monolog\Handler\StreamHandler(APPPATH.'Logs/app.log', \Monolog\Logger::WARNING);
-		$log->pushHandler($data);
+		//$log = new \Monolog\Logger('name');
+		//$data = new \Monolog\Handler\StreamHandler(APPPATH.'Logs/app.log', \Monolog\Logger::WARNING);
+		//$log->pushHandler($data); 
 		//$log->addWarning('Foo');
 	}
 	
